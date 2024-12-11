446b703628568c404fe43c1f11dc5d5ce05c7590
swaroop-12 <swaroop.k.g49@gmail.com>
2024-12-11 20:49:13 +0530

    done

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
-
+import path from 'path';
 
 dotenv.config();
 
@@ -19,7 +19,7 @@ mongoose
     console.log(err);
   });
 
-
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -30,18 +30,16 @@ app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
-app.get('/',(req,res)=>{
-  res.send('API IS WORKING')
-})
-
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -52,4 +50,3 @@ app.use((err, req, res, next) => {
     message,
   });
 });
-export default app;
