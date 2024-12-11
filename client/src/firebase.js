04fb568759112a0beed11a6ff24eb68854995bb1
swaroop-12 <swaroop.k.g49@gmail.com>
2024-12-11 18:13:55 +0530

    done

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "photo-blog-6756b.firebaseapp.com",
-  projectId: "photo-blog-6756b",
-  storageBucket: "photo-blog-6756b.firebasestorage.app",
-  messagingSenderId: "402148214318",
-  appId: "1:402148214318:web:693724b5fb43ea834a41bf"
+  authDomain: "photo-blog-792b3.firebaseapp.com",
+  projectId: "photo-blog-792b3",
+  storageBucket: "photo-blog-792b3.firebasestorage.app",
+  messagingSenderId: "533100528365",
+  appId: "1:533100528365:web:9f7993ef3ac58c3271c2e4"
 };
 
 // Initialize Firebase
